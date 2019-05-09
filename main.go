3f8054c5ce6ed7648146463e7a005dc2cc9afbe6
Elena Hopkins <ehopkins@enova.com>
2019-05-08 19:38:26 -0500

    adding control loop for game play

--- main.go
+++ main.go
@@ -2,25 +2,45 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/ladygogo/BattleGopher/gameboard"
 	"github.com/ladygogo/BattleGopher/player"
 )
 
 func main() {
-	g, err := gameboard.InitializeBoard(5)
-	fmt.Println(err)
-	fmt.Println(g.MaskedBoard)
-	fmt.Println(g.UnmaskedBoard)
-	fmt.Println(g.CheckForHit(2, 2))
-	fmt.Println(g.CheckForHit(0, 1))
-	fmt.Println(g.MaskedBoard)
-	fmt.Println(g.UnmaskedBoard)
-	fmt.Println("Are all gophers sunk?", g.AllGophersSunk())
-	g.PrettyPrint()
+	player1, _ := player.InitializePlayer("Linda")
+	player2, _ := player.InitializePlayer("Player 2")
+	playerArray := [...]player.Player{player1, player2}
 
-	player, err := player.InitializePlayer("Linda")
-	fmt.Println(player.Name, player.Score)
-	player.NewBoard(5)
-	player.Gameboard.PrettyPrint()
+	for i, _ := range playerArray {
+		playerArray[i].NewBoard(2)
+	}
+
+	fmt.Println("Welcome to Battle Gopher!")
+	fmt.Println("---------------------")
+
+	var row, column int
+	playerIndex := 0
+
+	for playerArray[0].Gameboard.AllGophersSunk() == false && playerArray[1].Gameboard.AllGophersSunk() == false {
+		fmt.Printf("%s's Turn\n", playerArray[(playerIndex)%2].Name)
+
+		playerArray[(playerIndex + 1)%2].Gameboard.PrettyPrint()	
+
+		fmt.Print("Enter a row-> ")
+	    fmt.Scanf("%d", &row)
+		fmt.Print("Enter a column-> ")
+	    fmt.Scanf("%d", &column)
+	
+	    hit, err := playerArray[(playerIndex + 1)%2].Gameboard.CheckForHit(row - 1, column - 1)
+
+	    if err != nil {
+	    	fmt.Println(err)
+	    	fmt.Println("Try again\n")
+	    } else {
+	    	playerArray[(playerIndex + 1)%2].Gameboard.PrettyPrint()	
+	    }
+
+	    if hit == false && err == nil {
+	    	playerIndex ++
+	    }
+	}
 }
