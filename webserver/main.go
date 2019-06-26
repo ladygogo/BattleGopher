ae0a132e198be370f10dcaf59f661a02ebb0522d
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2019-06-26 17:14:50 -0500

    Add webserver skeleton for API-Style meetup!

---
+++ webserver/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/ladygogo/BattleGopher/player"
+)
+
+func main() {
+	// Add router
+
+	// Add Handler Functions
+
+	// Start the webserver
+}
+
+// Add a Handler function with required arguments
+//func Example(w http.ResponseWriter, r *http.Request)
+
