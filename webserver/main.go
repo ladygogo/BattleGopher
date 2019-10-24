906019800a443e6ecf69210ca49ba8a36f2d7de0
Rebecca Luttmer <rluttmer@enova.com>
2019-10-23 19:49:28 -0500

    Built guess endpoint and it's awesome

--- webserver/main.go
+++ webserver/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -12,35 +12,38 @@ import (
 
 type Session struct {
 	playerArray []player.Player
-	gameId int
-// HERE: Need to track the index of the player who currently is eligible for a turn
+	gameId      int
+	turnId      int
 }
 
 type GameInput struct {
-	BoardDimension int `json:"board_dimension"`
-	PlayerNames []string `json:"player_names"`
+	BoardDimension int      `json:"board_dimension"`
+	PlayerNames    []string `json:"player_names"`
 }
 
 type SessionData struct {
 	SessionId int `json:"session_id"`
 }
 
+type GuessInput struct {
+	SessionId int `json:"session_id"`
+	PlayerId  int `json:"player_id"`
+	Row       int `json:"row"`
+	Col       int `json:"col"`
+}
+
 var SessionCount = 0
 var sessions []Session
 
-
-
 func main() {
-  // Initialize the sessions
-	initialLength := 1
-	arrayCapacity := 5
-	sessions = make([]Session, initialLength, arrayCapacity)
+	// Initialize the sessions
+	sessions = make([]Session, 0, 5)
 
-	      // Add router
+	// Add router
 	router := mux.NewRouter()
 
-	      // Add Handler Functions
-	router.HandleFunc("/guess", GuessHandler)
+	// Add Handler Functions
+	router.HandleFunc("/guess", GuessHandler).Methods("POST")
 	router.HandleFunc("/newgame", NewGameHandler).Methods("POST")
 	router.HandleFunc("/turn", TurnHandler)
 	// Separating Turns from Guesses because remote users don't know
@@ -56,20 +59,56 @@ func main() {
 //func Example(w http.ResponseWriter, r *http.Request)
 func GuessHandler(w http.ResponseWriter, r *http.Request) {
 	// Create struct to hold Guess inputs and unmarshal into struct
+	var guessInput GuessInput
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("errr is: ", err)
+	}
+	err = json.Unmarshal(body, &guessInput)
+	if err != nil {
+		fmt.Println("unmarshal errr is: ", err)
+	}
 	// Need session_id, player_id (who is guessing), row, col
 
-
 	// Retrieve session from sessions array
-
+	if guessInput.SessionId >= len(sessions) {
+		fmt.Println("Invalid session id")
+		return
+	}
+	session := &sessions[guessInput.SessionId]
 	// Ensure game is not over AND player_id matches eligible player's id (DetermineEligiblePlayer) and return if correct player did not guess
-
+	playerId := DetermineEligiblePlayer(*session)
+	if playerId != guessInput.PlayerId {
+		fmt.Println("Not this player's turn!!!")
+		return
+	}
 	// Take row and column, and use *other* player's CheckForHit function; check for error
-
+	gameboard := &session.playerArray[(playerId+1)%2].Gameboard
+	hit, err := gameboard.CheckForHit(guessInput.Row, guessInput.Col)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	// If CheckForHit returns true, check to see if all gophers are sunk on *other* players board
 	// Else If False, increment the turn counter (in the session) (if hit, get another turn)
-
-	// Return the API response (hit: true/false, game_over: true/false ?, masked_board1: [], masked_board2: [])
-
+	gameOver := false
+	if hit {
+		gameOver = gameboard.AllGophersSunk()
+	} else {
+		session.turnId++
+	}
+	// Return the API response (hit: true/false, game_over: true/false ?)
+	response := map[string]bool{
+		"hit": hit, "game_over": gameOver,
+	}
+	output, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(output)
 }
 
 func NewGameHandler(w http.ResponseWriter, r *http.Request) {
@@ -77,14 +116,14 @@ func NewGameHandler(w http.ResponseWriter, r *http.Request) {
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-	  fmt.Println("errr is: ", err)
+		fmt.Println("errr is: ", err)
 	}
 	err = json.Unmarshal(body, &gameInput)
 	if err != nil {
-	  fmt.Println("unmarshal errr is: ", err)
+		fmt.Println("unmarshal errr is: ", err)
 	}
 
-	session := Session{ gameId: SessionCount, playerArray: []player.Player{} }
+	session := Session{gameId: SessionCount, playerArray: []player.Player{}}
 	SessionCount++
 	for _, playerName := range gameInput.PlayerNames {
 		p, err := player.InitializePlayer(playerName)
@@ -120,7 +159,6 @@ func TurnHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Battle Gopher Turn Handler!")
 	fmt.Println("---------------------")
 
-
 }
 
 func APIDocHandler(w http.ResponseWriter, r *http.Request) {
@@ -132,6 +170,5 @@ func APIDocHandler(w http.ResponseWriter, r *http.Request) {
 
 // Create function that computes the index of the player who is eligible for a turn
 func DetermineEligiblePlayer(session Session) int {
-
-
+	return session.turnId % 2
 }
